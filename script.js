95af7d5c37b98c2dbf217ac4edac99c01f061f95
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2022-06-06 18:21:09 -0300

    acrescentando a opcao de colocar novos projetos pelo JS

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+function addProjects() {
+  const projects = [{name: 'lessons-learned', classes: 'project HTML/CSS'}];
+  const projectsGroup = document.querySelector('#projects-group');
+  for (let index = 0; index < projects.length; index += 1) {
+    const project = document.createElement('div');
+    project.style.backgroundImage = `url('images/${projects[index].name}.png')`;
+    project.className = projects[index].classes;
+    projectsGroup.appendChild(project);
+  }
+}
+
 function filterItems() {
-  const items = ['ALL', 'JS', 'HTML', 'CSS'];
+  const items = ['ALL', 'HTML/CSS'];
   const projectsFilters = document.querySelector('#projects-filters');
   for (let index = 0; index < items.length; index += 1) {
     const newFilter = document.createElement('li');
@@ -11,6 +22,7 @@ function filterItems() {
 
 function startFunction() {
   filterItems();
+  addProjects();
 }
 
 window.onload = startFunction;
