42e58e321d1f5119d483023b6402a70e80f03a0e
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2022-06-06 20:42:04 -0300

    adicionando as funcoes dos projetos

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
+function projectOptionsInformation(projectOptions, project) {
+  const projectName = document.createElement('h1');
+  projectName.innerText = project.name;
+  projectName.style.visibility = 'hidden';
+  projectOptions.appendChild(projectName);
+  const projectFilters = document.createElement('p');
+  projectFilters.innerText = project.classes;
+  projectFilters.style.visibility = 'hidden';
+  projectOptions.appendChild(projectFilters);
+  const projectButton = document.createElement('button');
+  projectButton.innerText = 'Show More';
+  projectButton.style.visibility = 'hidden';
+  projectButton.className = 'btn btn-outline-dark';
+  projectOptions.appendChild(projectButton);
+}
+
+function hiddenProjectDatails(project) {
+  const projectOptions = project.target.firstChild;
+  projectOptions.style.height = '0';
+  for (let index = 0; index < projectOptions.children.length; index += 1) {
+    projectOptions.children[index].style.visibility = 'hidden';
+  }
+}
+
+function showProjectDatails(project) {
+  const projectOptions = project.target.firstChild;
+  projectOptions.style.height = '100%';
+  for (let index = 0; index < projectOptions.children.length; index += 1) {
+    projectOptions.children[index].style.visibility = 'visible';
+  }
+}
+
 function addProjects() {
-  const projects = [{name: 'lessons-learned', classes: 'project HTML/CSS'}];
+  const projects = [{name: 'Lessons Learned', imageName: 'lessons-learned', classes: 'project HTML/CSS'}];
   const projectsGroup = document.querySelector('#projects-group');
   for (let index = 0; index < projects.length; index += 1) {
     const project = document.createElement('div');
-    project.style.backgroundImage = `url('images/${projects[index].name}.png')`;
+    project.style.backgroundImage = `url('images/${projects[index].imageName}.png')`;
     project.className = projects[index].classes;
+    project.addEventListener('mouseenter', showProjectDatails);
+    project.addEventListener('mouseleave', hiddenProjectDatails);
     projectsGroup.appendChild(project);
+    const projectOptions = document.createElement('div');
+    projectOptions.classList.add('project-options');
+    project.appendChild(projectOptions);
+    projectOptionsInformation(projectOptions, projects[index]);
   }
 }
 
