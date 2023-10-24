750a79d6d6aec291a31b178804f96ad8df46e61b
fnmatheus <nasc.matheusfrancisco@gmail.com>
2023-10-24 15:49:36 -0300

    feat: edit home image

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -5,29 +5,33 @@ import { Email, GitHub, LinkedIn } from './svgs/Index'
 
 export default function HomePage() {
   return (
-    <section className="pt-16">
-      <div>
-        <h1>Hello! I'm</h1>
-        <h1>Matheus</h1>
-      </div>
-      <p>
-        Venha solucionar seus problemas com SITES, LANDING PAGES e muito mais!
-      </p>
-      <div>
-        <button>saiba mais!</button>
-        <button>contate-me</button>
-      </div>
-      <Image src={Photo} alt="photo" />
-      <div className="flex w-full justify-center items-center gap-[52px]">
-        <a href="https://github.com/fnmatheus" target="_blank">
-          <GitHub className="text-[50px]" />
-        </a>
-        <a href="mailto:nasc.matheusfrancisco@gmail.com" target="_blank">
-          <Email className="text-[50px]" />
-        </a>
-        <a href="https://www.linkedin.com/in/fnmatheus/" target="_blank">
-          <LinkedIn className="text-[50px]" />
-        </a>
+    <section>
+      <div className="pt-16 flex flex-col h-screen justify-center items-center gap-10">
+        <div>
+          <h1>Hello! I'm</h1>
+          <h1>Matheus</h1>
+        </div>
+        <p>
+          Venha solucionar seus problemas com SITES, LANDING PAGES e muito mais!
+        </p>
+        <div className="flex flex-col">
+          <button>saiba mais!</button>
+          <button>contate-me</button>
+        </div>
+        <div className="rounded-full w-[218px] h-[218px] bg-gradient-to-br from-lightBlue to-purple flex justify-center items-center">
+          <Image src={Photo} alt="photo" className="rounded-full w-[214px] h-[214px]" />
+        </div>
+        <div className="flex w-full justify-center items-center gap-[52px]">
+          <a href="https://github.com/fnmatheus" target="_blank">
+            <GitHub className="text-[50px]" />
+          </a>
+          <a href="mailto:nasc.matheusfrancisco@gmail.com" target="_blank">
+            <Email className="text-[50px]" />
+          </a>
+          <a href="https://www.linkedin.com/in/fnmatheus/" target="_blank">
+            <LinkedIn className="text-[50px]" />
+          </a>
+        </div>
       </div>
     </section>
   )
