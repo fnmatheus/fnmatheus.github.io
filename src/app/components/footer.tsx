798aedcf05fe7bfdd9c7431f8f9cdecbc53e1e24
fnmatheus <nasc.matheusfrancisco@gmail.com>
2023-10-25 10:39:00 -0300

    feat: footer base HTML

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,10 @@
+import { WhiteLogo } from './svgs/Index';
+
+export default function Footer() {
+  return (
+    <footer>
+      <WhiteLogo />
+      <span>© fnmatheus</span>
+    </footer>
+  )
+}
--- src/app/components/svgs/Index.tsx
+++ src/app/components/svgs/Index.tsx
@@ -1,4 +1,5 @@
 export {default as Logo} from './logo.svg';
+export {default as WhiteLogo} from './logo1.svg';
 export {default as GitHub} from './github.svg';
 export {default as Email} from './envelope-solid.svg';
 export {default as LinkedIn} from './linkedin.svg';
