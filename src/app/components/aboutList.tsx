5795ca92a8b5c7e0ff1195255adf2c5d9978ef1a
fnmatheus <nasc.matheusfrancisco@gmail.com>
2023-10-25 09:50:21 -0300

    feat: about list filtter

--- src/app/components/aboutList.tsx
+++ src/app/components/aboutList.tsx
@@ -1,21 +1,44 @@
+"use client"
+
+import React, { useState } from 'react';
+
 export default function AboutList() {
+  const [list, setList] = useState(0);
+
+  const skills = ['JavaScript', 'React (Next.js)', 'Node.js', 'Express', 'MySQL', 'MongoDB', 'HTML'];
+  const studies = ['Trybe (FullStack)']
+  const certificates = ['Front-end (Trybe)', 'BackEnd(Trybe)', 'Computer Science (Trybe)']
+
+  const handleButton = (e: React.MouseEvent) => {
+    e.preventDefault();
+    const target = e.target as HTMLButtonElement;
+
+    if(target.id === 'skills') setList(0);
+    else if(target.id == 'studies') setList(1);
+    else setList(2);
+  };
+
   return (
     <section>
       <div>
-        <h3>Skills</h3>
-        <h3>Estudos</h3>
-        <h3>Certificados</h3>
+        <button id='skills' onClick={handleButton}>Skills</button>
+        <button id='studies' onClick={handleButton}>Estudos</button>
+        <button id='certificates' onClick={handleButton}>Certificados</button>
       </div>
       <div>
         <ul>
-          <li>JavaScript</li>
-          <li>{'React (Next.js)'}</li>
-          <li>Node.js</li>
-          <li>Express</li>
-          <li>MySQL</li>
-          <li>MongoDB</li>
-          <li>HTML</li>
-          <li>CSS</li>
+          { 
+            list == 0 &&
+            skills.map(skill => <li>{ skill }</li>)
+          }
+          { 
+            list == 1 &&
+            studies.map(studie => <li>{ studie }</li>)
+          }
+          { 
+            list == 2 &&
+            certificates.map(certificate => <li>{ certificate }</li>)
+          }
         </ul>
       </div>
     </section>
