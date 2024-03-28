1e63a8b5d917ee46881d88f274342142ad10c1d8
fnmatheus <nasc.matheusfrancisco@gmail.com>
2024-03-28 09:39:26 -0300

    feat: get and list from google sheets

--- src/app/components/aboutList.tsx
+++ src/app/components/aboutList.tsx
@@ -1,21 +1,34 @@
 "use client"
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { IHardSkillsList } from '../utils/interfaces';
+
+const hardskillsUrl = 'https://script.google.com/macros/s/AKfycbzorQBBFngf4LR645uShRF_eUkI_825_sfTcRhRBGz5_ftTURDjTeDmq62rjLM4grsZ/exec';
 
 export default function AboutList() {
-  const [list, setList] = useState(0);
+  const [list, setList] = useState<IHardSkillsList>();
+  const [selected, setSelected] = useState('skills');
 
-  const skills = ['JavaScript', 'React (Next.js)', 'Node.js', 'Express', 'MySQL', 'MongoDB', 'HTML'];
-  const studies = ['Trybe (FullStack)']
-  const certificates = ['Front-end (Trybe)', 'BackEnd(Trybe)', 'Computer Science (Trybe)']
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(hardskillsUrl, {method: 'GET'});
+        const data = await response.json();
+        setList(data);
+      } catch (error) {
+        console.log('error');
+      }
+    };
+    getData();
+  }, []);
 
   const handleButton = (e: React.MouseEvent) => {
     e.preventDefault();
     const target = e.target as HTMLButtonElement;
 
-    if(target.id === 'skills') setList(0);
-    else if(target.id == 'studies') setList(1);
-    else setList(2);
+    if(target.id === 'skills') setSelected('skills');
+    else if(target.id == 'studies') setSelected('studies');
+    else setSelected('certificates');
   };
 
   return (
@@ -23,30 +36,22 @@ export default function AboutList() {
       <div className="w-full flex justify-between font-bold text-lg">
         <button id='skills' onClick={handleButton} className="flex flex-col justify-center items-center">
           Skills
-          <div className={`w-[40px] h-[3px] ${(list !== 0) ? 'bg-transparent' : 'bg-purple'} rounded-full`} />
+          <div className={`w-[40px] h-[3px] ${(selected !== 'skills') ? 'bg-transparent' : 'bg-purple'} rounded-full`} />
         </button>
         <button id='studies' onClick={handleButton} className="flex flex-col justify-center items-center">
           Estudos
-          <div className={`w-[40px] h-[3px] ${(list !== 1) ? 'bg-transparent' : 'bg-purple'} rounded-full`} />
+          <div className={`w-[40px] h-[3px] ${(selected !== 'studies') ? 'bg-transparent' : 'bg-purple'} rounded-full`} />
         </button>
         <button id='certificates' onClick={handleButton} className="flex flex-col justify-center items-center">
           Certificados
-          <div className={`w-[40px] h-[3px] ${(list !== 2) ? 'bg-transparent' : 'bg-purple'} rounded-full`} />
+          <div className={`w-[40px] h-[3px] ${(selected !== 'certificates') ? 'bg-transparent' : 'bg-purple'} rounded-full`} />
         </button>
       </div>
       <div className="h-[180px]">
         <ul className="list-inside list-disc">
-          { 
-            list == 0 &&
-            skills.map(skill => <li key={skill}>{ skill }</li>)
-          }
-          { 
-            list == 1 &&
-            studies.map(studie => <li key={studie}>{ studie }</li>)
-          }
-          { 
-            list == 2 &&
-            certificates.map(certificate => <li key={certificate}>{ certificate }</li>)
+          {
+            list !== undefined &&
+            list[selected as keyof IHardSkillsList].map(item => <li key={item.name}>{ item.name }</li>)
           }
         </ul>
       </div>
